3fa82c04921a46d9e7f840287feca3cf09b37703
Guruprasad <dv.guruprasad@gmail.com>
2012-07-11 15:30:33 +0530

    initial commit

---
+++ front-end/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FrontEnd::Application.config.secret_token = '6b9e366c49f45c6e9d55406a426bf9377d1f664a2a881235cb5dc517970e60ac87268dc059306d929e961a8908e64cf96ff1db5eadefa5cebdb497501f221c43'
