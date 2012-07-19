d9e10a9ae871a10572efd7a88156fe773aefd606
Srihari Srinivasan <srihari.srinivasan@gmail.com>
2012-07-19 15:18:36 +0530

    added a deduplication stage

--- back-end/test/com/tidings/backend/PipelineTest.java
+++ back-end/test/com/tidings/backend/PipelineTest.java
@@ -17,16 +17,20 @@ public class PipelineTest {
         Pipeline pipeline = new Pipeline();
         final MemoryChannel<Message> crawlInbox = new MemoryChannel<Message>();
         MemoryChannel<Message> transformInbox = new MemoryChannel<Message>();
+        MemoryChannel<Message> dedupInbox = new MemoryChannel<Message>();
         MemoryChannel<Message> loaderInbox = new MemoryChannel<Message>();
         ThreadFiber crawlWorker = new ThreadFiber();
         ThreadFiber transformWorker = new ThreadFiber();
+        ThreadFiber dedupWorker = new ThreadFiber();
         ThreadFiber loadingWorker = new ThreadFiber();
 
         FeedCrawlStage crawlStage = new FeedCrawlStage(crawlInbox, transformInbox, crawlWorker);
-        TransformStage trasformStage = new TransformStage(transformInbox, loaderInbox, transformWorker);
+        TransformStage trasformStage = new TransformStage(transformInbox, dedupInbox, transformWorker, new NewsTransformer());
+        DeduplicationStage deduplicationStage = new DeduplicationStage(dedupInbox, loaderInbox, dedupWorker, new NewsItemsRepository());
         LoadingStage loadingStage = new LoadingStage(loaderInbox, null, loadingWorker, new NewsItemsRepository());
         pipeline.addStage(crawlStage);
         pipeline.addStage(trasformStage);
+        pipeline.addStage(deduplicationStage);
         pipeline.addStage(loadingStage);
         pipeline.start();
         crawlInbox.publish(new Message(bigDataFeeds()));
@@ -61,11 +65,26 @@ public class PipelineTest {
         String[] feeds = {"http://allthingsd.com/tag/big-data/feed/",
                 "http://feeds.feedburner.com/TdwiChannel-BigDataAnalytics",
                 "http://blogs.teradata.com/rss/",
+                "http://datastori.es/feed/",
                 "http://hortonworks.com/feed/",
                 "http://www.zdnet.com/blog/big-data/rss.xml",
                 "http://feeds.feedburner.com/QlikviewMarketingIntelligenceBlog",
                 "http://www.thebigdatainsightgroup.com/site/rss/articles/all",
                 "http://www.smartercomputingblog.com/category/big-data/feed/",
+                "http://bigdatablog.emc.com/?feed=rss2",
+                "http://blogs.cisco.com/tag/big-data/feed",
+                "http://beautifuldata.net/feed/",
+                "http://www.clusterseven.com/ralph-baxters-big-data-blog/rss.xml",
+                "http://www.clusterseven.com/press-releases/rss.xml",
+                "http://blogs.splunk.com/feed/",
+                "http://feeds2.feedburner.com/WikibonBlog",
+                "http://www.appdynamics.com/blog/feed/",
+                "http://intelligentbusiness.biz/wordpress/?feed=rss2",
+                "http://feeds.feedburner.com/hkotadia/qkGh",
+                "http://www.cubrid.org/blog/rss",
+                "http://feeds.feedburner.com/smartdatacollective_allposts",
+                "http://feeds.feedburner.com/typepad/petewarden",
+                "http://marcandrews.typepad.com/marc_andrews/atom.xml",
         };
         return Arrays.asList(feeds);
     }
