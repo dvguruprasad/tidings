b8e97c9e3dbef54386d956399e41281335ecc62b
Guruprasad <dv.guruprasad@gmail.com>
2012-09-04 11:30:32 +0530

    showing category links and making the currently selected category active

--- front-end/app/controllers/news_controller.rb
+++ front-end/app/controllers/news_controller.rb
@@ -1,7 +1,8 @@
 class NewsController < ApplicationController
   def index
+    @categories = Category.all
     if params[:category]
-      @category = params[:category]
+      @current_category = params[:category]
       @news_items = NewsItem.where(:category => params[:category].camelize).order(:published_date).page params[:page]
     else
       @news_items = NewsItem.order(:published_date).page params[:page]
